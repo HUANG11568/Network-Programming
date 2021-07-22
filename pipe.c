dc7a803b2baebfe6bfeb4a1eec31fd3a9707bac7
HUANG11568 <huang_hg97@163.com>
2021-07-22 22:36:18 +0800

    modify pipe.c

--- pipe.c
+++ pipe.c
@@ -8,6 +8,7 @@ int main()
     pid_t pid;
     char buf[MAXBUF] = {};
     char input[] = "hello pipe!";
+    char input2[] = "i am fine! and you?";
     int read_len = 0;
 
     /*管道初始化*/
@@ -18,13 +19,20 @@ int main()
     {
         /*子进程*/
         write(fd[1], input, sizeof(input));
+        sleep(2); /*管道内的数据为无主数据， 任何人均可读走， 此处保证父进程能读到数据*/
+
+        read_len = read(fd[0], buf, MAXBUF);
+        //puts(buf);
+        printf("child buf:%s, readlen:%d\n", buf, read_len);
     }
     else
     {
         /*父进程*/
         read_len = read(fd[0], buf, MAXBUF);
-        puts(buf);
-        printf("buf:%s, readlen:%d\n", buf, read_len);
+        write(fd[1], input2, sizeof(input2));
+        //puts(buf);
+        printf("father buf:%s, readlen:%d\n", buf, read_len);
+        sleep(3);
     }
 
     return 0;
